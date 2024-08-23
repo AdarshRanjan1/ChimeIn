8446f8ce5f9aecac56bfbfc48e78ec79b0ebf1bf
Adarsh Ranjan <adarshranjan8427@gmail.com>
2024-08-23 19:20:13 +0530

    3rd-part complete

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation';
 
-const Converstaion = () => {
+const Conversation = ({conversation, lastIdx, emoji}) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+
+  
   return (
     <>
-      <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+      <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+        ${isSelected ? "bg-sky-500" : ""}
+        `}
+        onClick={() => setSelectedConversation(conversation)}
+        >
         <div className='avatar online'>
           <div className='w-12 rounded-full'>
             <img
-              src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' 
+              src={conversation.profilePic}
               alt='user avatar' 
             />
           </div>
@@ -15,17 +25,17 @@ const Converstaion = () => {
 
         <div className='flex flex-col flex-1'>
           <div className='flex gap-3 justify-between'>
-            <p className='font-bold text-gray-200'>John Doe</p>
-            <span className='text-xl'>P</span>
+            <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
           </div>
         </div>
       </div>
-      <div className='divider my-0 py-0 h-1' />
+      {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     </>
   )
 }
 
-export default Converstaion
+export default Conversation
 
 
 /* Starter code for conversation
--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-const GenderCheckBox = () => {
+const GenderCheckBox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className='flex'>
       <div className='form-control'>
-        <label className={`cursor-pointer label`}>
+        <label className={`cursor-pointer label ${selectedGender==="male" ? "selected" : ""}`}>
           <span className='label-text'>Male</span>
-          <input type='checkbox' className='checkbox checkbox-info' />
+          <input type='checkbox' className='checkbox checkbox-info' 
+            checked={selectedGender==="male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className='form-control'>
-        <label className={`cursor-pointer label`}>
+        <label className={`cursor-pointer label ${selectedGender==="female" ? "selected" : ""}`}>
           <span className='label-text'>Female</span>
-          <input type='checkbox' className='checkbox checkbox-secondary' />
+          <input type='checkbox' className='checkbox checkbox-secondary' 
+            checked={selectedGender==="female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
