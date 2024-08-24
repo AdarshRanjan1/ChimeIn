27f55f0a56190f5a28ab999c0b589281fc79685f
Adarsh Ranjan <adarshranjan8427@gmail.com>
2024-08-24 09:13:53 +0530

    4th part completed - socket

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js"; 
 import Message from "../models/message.model.js";
+import { getReceiverSocketId,io } from "../socket/socket.js";
 
 export const sendMessage = async (req,res) => {
   try {
@@ -27,14 +28,19 @@ export const sendMessage = async (req,res) => {
       converstaion.messages.push(newMessage._id);
     }
 
-    // SOCKET IO Functionality 
-
+    
     // await converstaion.save();
     // await newMessage.save();
 
     //to make it optimize
     await Promise.all([converstaion.save(), newMessage.save()]);
     // this makes both the processes run parallel
+
+    // SOCKET IO Functionality 
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
 
